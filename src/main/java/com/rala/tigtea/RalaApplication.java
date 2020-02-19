e36f7aee40537c8791cb0926147813eccc6bbdff
nara <shinnara26@gmail.com>
2020-02-19 21:11:58 +0900

    초기셋팅

---
+++ src/main/java/com/rala/tigtea/RalaApplication.java
@@ -0,0 +1,13 @@
+package com.rala.tigtea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rala/tigtea/RalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rala.tigtea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
